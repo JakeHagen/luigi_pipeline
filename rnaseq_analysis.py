26d789d31c525726e9aab06034e6af582d215cfa
Jake Hagen <jacob.hagen@mssm.edu>
2015-10-26 17:42:17 -0400

    cleaned up code, hopefully more readable. Still need to work on diff exp part

--- rnaseq_analysis.py
+++ rnaseq_analysis.py
@@ -3,120 +3,113 @@ import subprocess
 import os
 import rpy2.robjects as robjects                                            
 import pandas as pd 
-#class params(luigi.Task):
 
-    #will output will be, can be pointed at an existing folder or the pipeline will create it
-wkdir = '/sc/orga/projects/argmac01a/luigi_rna_ensembl37'
-    # Only needed if STAR genome needs to be generated
-genome_fasta = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa' 
-genome_gtf = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/Homo_sapiens.GRCh37.75.gtf'
-
-"""Either where the STAR genome is located or were it will be created
-IMPORTANT: if the STAR genome needs to be created, do not create the folder,
-Let the pipeline create it."""
-star_genome_folder = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/star_genome_ensembl37_99'
-
-    # RNAseq experiment info
-read_length = 100
-stranded = 0
-    #paried = ?
-    #prep = polyA or ribozero
-
-    # Should be multiple of samples
-cores = 6
-
-"""
-s01:/sc/orga/projects/argmac01a/QC_C211.B857_huh7_DCPS.SE.RNASeqRibozero.RAPiD.Human/fastqs/lsi1_CAGATC_L008_R1_001.C6673ACXX.fastq.gz
-s02:/sc/orga/projects/argmac01a/QC_C211.B857_huh7_DCPS.SE.RNASeqRibozero.RAPiD.Human/fastqs/lsi2_ATCACG_L008_R1_001.C6673ACXX.fastq.gz
-s03:/sc/orga/projects/argmac01a/QC_C211.B857_huh7_DCPS.SE.RNASeqRibozero.RAPiD.Human/fastqs/lsi3_TCGGCA_L008_R1_001.C6673ACXX.fastq.gz
-"""
-
-class params(luigi.Task):
+class parameters(luigi.Task):
+    """class to take all parameters
+    fastq_file needs to be in the format specified below, if the experiment is 
+    paired end, the second fastq file should be after the first, only seperated 
+    by a space
     
-    fq = luigi.Parameter()
-    wkdir = luigi.Parameter(default = '/sc/orga/projects/argmac01a/luigi_rna_ensembl37')                       
-    # Only needed if STAR genome needs to be generated                          
+    s01:/sc/orga/projects/argmac01a/QC_C211.B857_huh7_DCPS.SE.RNASeqRibozero.RAPiD.Human/fastqs/lsi1_CAGATC_L008_R1_001.C6673ACXX.fastq.gz
+    s02:/sc/orga/projects/argmac01a/QC_C211.B857_huh7_DCPS.SE.RNASeqRibozero.RAPiD.Human/fastqs/lsi2_ATCACG_L008_R1_001.C6673ACXX.fastq.gz
+    s03:/sc/orga/projects/argmac01a/QC_C211.B857_huh7_DCPS.SE.RNASeqRibozero.RAPiD.Human/fastqs/lsi3_TCGGCA_L008_R1_001.C6673ACXX.fastq.gz
+    """
+    
+    fastq_file = luigi.Parameter(
+                    default = '/hpc/users/hagenj02/luigi_pipeline/fastqfile')
+    
+    wkdir = luigi.Parameter(default = os.getcwd())
+
     genome_fasta = luigi.Parameter(
-            default = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa'
+            default = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/'
+                        'Homo_sapiens.GRCh37.75.dna.primary_assembly.fa'
                                    )
     genome_gtf = luigi.Parameter(
-            default = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/Homo_sapiens.GRCh37.75.gtf'
+            default = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/'
+                        'Homo_sapiens.GRCh37.75.gtf'
                                  )
     star_genome_folder = luigi.Parameter(
-            default = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/star_genome_ensembl37_99'
+            default = '/sc/orga/projects/Houton_Sander/genomes/ensembl37/'
+                        'star_genome_ensembl37_99'
                                          )
     read_length = luigi.IntParameter(default = 100)
     stranded = luigi.IntParameter(default = 0)
-    #paried = ?
-    #prep = polyA or ribozero                                             
+    paried = luigi.IntParameter(default = 0)                                            
     cores = luigi.IntParameter(default = 6)    
 
-pm = params
 
 class fastqs(luigi.Task):
     
     def requires(self):
-        return params()
-    def output(self):
-        d = {}
-        with open(pm().fq) as f:
+        return parameters()
+    
+    def run(self):    
+        fastq_dic = {}
+        with open(parameters().fastq_file) as f:
             for line in f:
-                key,val = line.strip().split(":")
-                d[key] = val  
-        return d
+                sample,path = line.strip().split(":")
+                fastq_dic[sample] = path
+        return fastq_dic
+    
+    def output(self):
+        return {sample:luigi.LocalTarget(path) for sample,path in self.run().items()} 
+
 
 class index_STAR_genome(luigi.Task):
     
     def requires(self):
-        return fastqs()
+        return parameters()
 
     def run(self):
-        if not os.path.exists(pm().star_genome_folder):
-            os.makedirs(pm().star_genome_folder) 
+        if not os.path.exists(parameters().star_genome_folder):
+            os.makedirs(parameters().star_genome_folder) 
         star_command = [
                         'STAR',
-                        '--runThreadN %d' % pm.cores,
+                        '--runThreadN %d' % parameters().cores,
                         '--runMode genomeGenerate',
-                        '--genomeDir %s' % pm().star_genome_folder,
-                        '--genomeFastaFiles %s' % pm().genome_fasta,
-                        '--sjdbGTFfile %s' % pm().genome_gtf,
-                        '--sjdbOverhang %d' % (pm.read_length - 1)
+                        '--genomeDir %s' % parameters().star_genome_folder,
+                        '--genomeFastaFiles %s' % parameters().genome_fasta,
+                        '--sjdbGTFfile %s' % parameters().genome_gtf,
+                        '--sjdbOverhang %d' % (parameters.read_length - 1)
                         ]
         star = subprocess.Popen(star_command)
         star.wait()
         if star.returncode != 0:
-            subprocess.call(['rm -rf %s' % pm().star_genome_folder])
-        os.rename('Log.out', '%s/Log.out' % pm().star_genome_folder)
+            subprocess.call(['rm -rf %s' % parameters().star_genome_folder])
+        os.rename('Log.out', '%s/Log.out' % parameters().star_genome_folder)
     def output(self):
-        return luigi.LocalTarget('%s/Log.out' % pm().star_genome_folder)
+        return luigi.LocalTarget('%s/Log.out' % parameters().star_genome_folder)
 
 
 class wk_dir(luigi.Task):
     
     def requires(self):
         return index_STAR_genome()
+    
     def run(self):
-        os.makedirs(pm().wkdir)
+        os.makedirs(parameters().wkdir)
+    
     def output(self):
-        return luigi.LocalTarget(pm().wkdir)
+        return luigi.LocalTarget(parameters().wkdir)
 
 
 class star_align(luigi.Task):
     
     sample = luigi.Parameter()
-    path = luigi.Parameter()
+    file_location = luigi.Parameter()
     def requires(self):
-        return index_STAR_genome(), wk_dir()
+        return index_STAR_genome(), wk_dir(), fastqs()
 
     def run(self):
-        os.makedirs('%s/%s/star' % (pm().wkdir, self.sample))
+        if not os.path.exists('%s/%s/star' % (parameters().wkdir, self.sample)):             
+            os.makedirs('%s/%s/star' % (parameters().wkdir, self.sample))
         s_command = [
                 'STAR',
-                    '--genomeDir %s' % pm().star_genome_folder,
+                    '--genomeDir %s' % parameters().star_genome_folder,
                     #'--sjdbGTFfile %s' % genome_gtf,
-                    '--readFilesIn %s' % self.path,
+                    '--readFilesIn %s' % self.file_location.path,
                     '--readFilesCommand zcat',
-                    '--runThreadN %d' % pm.cores, #(cores/len(fastq_dictionary)),
+                    '--runThreadN %d' % parameters().cores, #(cores/len(fastq_dictionary)),
                     '--outSAMmode Full',
                     '--outReadsUnmapped Fastx',
                     '--chimSegmentMin 15',
@@ -127,36 +120,43 @@ class star_align(luigi.Task):
                     #'--genomeLoad LoadAndRemove',
                     #'--limitBAMsortRAM 15000000000',
                     '--outSAMtype BAM SortedByCoordinate',#Unsorted
-                    '--outFileNamePrefix %s/%s/star/%s.' % (pm().wkdir, self.sample, self.sample)
+                    '--outFileNamePrefix %s/%s/star/%s.' % (parameters().wkdir, self.sample, self.sample)
                     ]
         star = subprocess.Popen(s_command)
         star.wait()
-        # Below removes folder if star command was unsuccessful 
-        # (files remain when star fails)
-        # renaming bam insures that if batch job gets cut while star is running
-        # the task will not be marked as complete 
+
+        '''Below removes folder if star command was unsuccessful 
+        (files remain when star fails)
+        renaming bam insures that if batch job gets cut while star is running
+        the task will not be marked as complete 
+        '''
         if star.returncode != 0:
-            subprocess.call(['rm', '-rf', '%s/%s/star' % (pm().wkdir, self.sample)])
+            subprocess.call(['rm', '-rf', '%s/%s/star' 
+                                % (parameters().wkdir, self.sample)])
         else:
-            os.rename('%s/%s/star/%s.Aligned.sortedByCoord.out.bam' % (pm().wkdir, self.sample, self.sample), '%s/%s/star/%s.bam' % (pm().wkdir, self.sample, self.sample))
+            os.rename('%s/%s/star/%s.Aligned.sortedByCoord.out.bam' 
+                        % (parameters().wkdir, self.sample, self.sample), 
+                        '%s/%s/star/%s.bam' 
+                            % (parameters().wkdir, self.sample, self.sample))
     def output(self):
-        return luigi.LocalTarget('%s/%s/star/%s.bam' % (pm().wkdir, self.sample, self.sample))
+        return luigi.LocalTarget('%s/%s/star/%s.bam' 
+                                    % (parameters().wkdir, self.sample, self.sample))
 
 
 class all_star_align(luigi.Task):
     
     def requires(self):
-        inpt = {}
-        for s,p in fastqs().output().items():
-            inpt[s] = star_align(sample = s, path = p)
-        return inpt
-
+        fastq_dict = fastqs().output()
+        return {
+                s:star_align(sample = s, file_location = p) 
+                    for s,p in fastqs_dict.items()
+                }
     def output(self):       
-        return {x:self.input()[x] for x in self.input()} 
+        bam_dict = self.input()
+        return bam_dict 
 
 
 class featureCounts(luigi.Task):
-    
     sample = luigi.Parameter()
     bam_file = luigi.Parameter()
     
@@ -164,7 +164,7 @@ class featureCounts(luigi.Task):
         return all_star_align()
 
     def run(self):
-        fc_wkdir = '%s/%s/featureCounts' % (wkdir, self.sample)
+        fc_wkdir = '%s/%s/featureCounts' % (parameters().wkdir, self.sample)
         if not os.path.exists(fc_wkdir):
             os.makedirs(fc_wkdir)
         featureCounts_command = [
@@ -174,18 +174,20 @@ class featureCounts(luigi.Task):
                                     '-s', '%d' % stranded,
                                     '-a', '%s' % genome_gtf,
                                     '-o', '%s/%s.prelim.counts' % (fc_wkdir,self.sample),
-                                    self.bam_file
+                                    self.bam_file.path
                                 ]
         fC = subprocess.Popen(featureCounts_command)
         fC.wait()
         if fC.returncode != 0:
             subprocess.call(['rm', '-rf', fc_wkdir])
         else:
-            os.rename('%s/%s.prelim.counts' % (fc_wkdir, self.sample), '%s/%s.counts' % (fc_wkdir, self.sample))
-            os.rename('%s/%s.prelim.counts.summary' % (fc_wkdir, self.sample), '%s/%s.counts.summary' % (fc_wkdir, self.sample))
+            os.rename('%s/%s.prelim.counts' % (fc_wkdir, self.sample), 
+                        '%s/%s.counts' % (fc_wkdir, self.sample))
+            os.rename('%s/%s.prelim.counts.summary' % (fc_wkdir, self.sample), 
+                        '%s/%s.counts.summary' % (fc_wkdir, self.sample))
 
     def output(self):
-        fc_wkdir = '%s/%s/featureCounts' % (wkdir, self.sample)
+        fc_wkdir = '%s/%s/featureCounts' % (parameters().wkdir, self.sample)
         return luigi.LocalTarget('%s/%s.counts' % (fc_wkdir, self.sample))
 
 
@@ -193,16 +195,18 @@ class all_featureCounts(luigi.Task):
 
     def requires(self):
         bam_dict = all_star_align().output() 
-        return {s:featureCounts(sample = s, bam_file = b.path) for s,b in bam_dict.items()}             
+        return {
+                s:featureCounts(sample = s, bam_file = b.path) 
+                    for s,b in bam_dict.items()
+                }             
 
-    def output(self):        
-        return {x:self.input()[x] for x in self.input()}
+    def output(self):
+        counts_dict = self.input()
+        return counts_dict
 
 
 class diff_exp_analysis(luigi.Task):
-    #import rpy2.robjects as robjects
-    #import pandas as pd
-
+    
     def requires(self):
         return all_featureCounts()
 
